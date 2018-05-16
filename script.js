491e02e0e60de8a2f6519d1d018f05a4449112db
Vampired <xdarkeylol@gmail.com>
2018-05-17 00:20:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function load() {
-    const progres = document.querySelector('.progres')
-    let width = 1;
-    let id = setInterval(frame, 20);
-    const arrow = document.getElementById("arrow");
-    const wait = document.querySelector("h4");
-        function frame() {
-            if(width >= 100){
-                clearInterval(id);
-            }
-            else {
-                width++;
-                progres.innerHTML = width + "%";
-                progres.style.width = width + '%';
-                if(width == 100){
-                    arrow.style.display = "block";
-                    wait.style.display = "none";
-                }
-            }
-        }
+	const progres = document.querySelector('.progres')
+	let width = 1;
+	let id = setInterval(frame, 20);
+	const arrow = document.getElementById("arrow");
+	const wait = document.querySelector("h4");
+
+	function frame() {
+		if (width >= 100) {
+			clearInterval(id);
+		} else {
+			width++;
+			progres.innerHTML = width + "%";
+			progres.style.width = width + '%';
+			if (width == 100) {
+				arrow.style.display = "block";
+				wait.style.display = "none";
+			}
+		}
+	}
 }
 
 window.addEventListener("load", load);
 
 // jQuery
 $("#arrow").on("click", function () {
-  $("body,html").animate({
-    scrollTop: $(".headerBar").offset().top
-  }, 1000)
+	$("body,html").animate({
+		scrollTop: $(".headerBar").offset().top
+	}, 1000)
 })
